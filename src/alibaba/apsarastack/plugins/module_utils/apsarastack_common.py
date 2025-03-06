0233eb61fb33bfcc374da7e0f7f6e1b5279ac1ea
儆钰 <jingyu.wy@alibaba-inc.com>
2025-03-06 20:22:17 +0800

    初始化项目结构

---
+++ src/alibaba/apsarastack/plugins/module_utils/apsarastack_common.py
@@ -0,0 +1,27 @@
+# encoding: utf-8
+'''
+Created on 2025年3月4日
+
+@author: jingyu.wy
+'''
+
+from ansible.module_utils.basic import env_fallback
+
+
+def common_argument_spec():
+    return dict(
+        apsarastack_domain=dict(aliases=['domain', 'popgw_domain'], no_log=True,
+                                 fallback=(env_fallback, ['APSARASTACK_DOMAIN', 'APSARASTACK_POPGW_DOMAIN'])),
+        apsarastack_access_key=dict(aliases=['access_key_id', 'access_key'], no_log=True,
+                                 fallback=(env_fallback, ['APSARASTACK_ACCESS_KEY', 'APSARASTACK_ACCESS_KEY_ID'])),
+        apsarastack_secret_key=dict(aliases=['secret_access_key', 'secret_key'], no_log=True,
+                                 fallback=(env_fallback, ['APSARASTACK_SECRET_KEY', 'APSARASTACK_SECRET_ACCESS_KEY'])),
+        apsarastack_security_token=dict(aliases=['security_token'], no_log=True,
+                                     fallback=(env_fallback, ['APSARASTACK_SECURITY_TOKEN'])),
+        apsarastack_region=dict(required=True, aliases=['region', 'region_id'],
+                                 fallback=(env_fallback, ['APSARASTACK_REGION', 'APSARASTACK_REGION_ID'])),
+        apsarastack_resourcegroupset=dict(required=True, aliases=['resourcegroupset', 'resourcegroupset_id', ],
+                                 fallback=(env_fallback, ['APSARASTACK_RESOURCE_GROUP', 'APSARASTACK_RESOURCE_GROUP_ID'])),
+        ecs_role_name=dict(aliases=['role_name'], fallback=(env_fallback, ['APSARASTACK_ECS_ROLE_NAME'])),
+        apsarastack_protocol=dict(aliases=['protocol'], fallback=(env_fallback, ['APSARASTACK_PROTOCOL']), default='http')
+    )
---
+++ src/alibaba/apsarastack/plugins/module_utils/apsarastack_connections.py
@@ -0,0 +1,135 @@
+# encoding: utf-8
+'''
+Created on 2025年3月4日
+
+@author: jingyu.wy
+'''
+import json
+import os
+
+try:
+    import footmark
+    import footmark.ecs
+    import footmark.slb
+    import footmark.vpc
+    import footmark.rds
+    import footmark.ess
+    import footmark.sts
+    import footmark.dns
+    import footmark.ram
+    import footmark.ros
+    import footmark.oos
+    import footmark.market
+    HAS_FOOTMARK = True
+except ImportError:
+    HAS_FOOTMARK = False
+
+
+class AnsibleACSError(Exception):
+    pass
+
+
+def get_acs_connection_info(params):
+
+    return dict(
+        acs_access_key_id=params.get('apsarastack_access_key'),
+        acs_secret_access_key=params.get('apsarastack_secret_key'),
+        security_token=params.get('apsarastack_security_token'),
+        ecs_role_name=params.get('ecs_role_name'),
+        user_agent='Ansible-Provider-Apsarastack',
+    )
+
+
+def get_assume_role(params):
+    """ Return new params """
+    sts_params = get_acs_connection_info(params)
+    assume_role = {}
+    if params.get('assume_role'):
+        assume_role['apsarastack_assume_role_arn'] = params['assume_role'].get('role_arn')
+        assume_role['apsarastack_assume_role_session_name'] = params['assume_role'].get('session_name')
+        assume_role['apsarastack_assume_role_session_expiration'] = params['assume_role'].get('session_expiration')
+        assume_role['apsarastack_assume_role_policy'] = params['assume_role'].get('policy')
+
+    assume_role_params = {
+        'role_arn': params.get('apsarastack_assume_role_arn') if params.get('apsarastack_assume_role_arn') else assume_role.get('apsarastack_assume_role_arn'),
+        'role_session_name': params.get('apsarastack_assume_role_session_name') if params.get('apsarastack_assume_role_session_name')
+        else assume_role.get('apsarastack_assume_role_session_name'),
+        'duration_seconds': params.get('apsarastack_assume_role_session_expiration') if params.get('apsarastack_assume_role_session_expiration')
+        else assume_role.get('apsarastack_assume_role_session_expiration', 3600),
+        'policy': assume_role.get('apsarastack_assume_role_policy', {})
+    }
+
+    try:
+        sts = connect_to_acs(footmark.sts, params.get('apsarastack_region'), **sts_params).assume_role(**assume_role_params).read()
+        sts_params['acs_access_key_id'], sts_params['acs_secret_access_key'], sts_params['security_token'] \
+ = sts['access_key_id'], sts['access_key_secret'], sts['security_token']
+    except AnsibleACSError as e:
+        params.fail_json(msg=str(e))
+    return sts_params
+
+
+def get_profile(params):
+    if not params['apsarastack_access_key'] and not params['ecs_role_name'] and params['profile']:
+        path = params['shared_credentials_file'] if params['shared_credentials_file'] else os.getenv('HOME') + '/.apsarastack/config.json'
+        auth = {}
+        with open(path, 'r') as f:
+            for pro in json.load(f)['profiles']:
+                if params['profile'] == pro['name']:
+                    auth = pro
+        if auth:
+            if auth['mode'] == 'AK' and auth.get('access_key_id') and auth.get('access_key_secret'):
+                params['apsarastack_access_key'] = auth.get('access_key_id')
+                params['apsarastack_secret_key'] = auth.get('access_key_secret')
+                params['apsarastack_region'] = auth.get('region_id')
+                params = get_acs_connection_info(params)
+            elif auth['mode'] == 'StsToken' and auth.get('access_key_id') and auth.get('access_key_secret') and auth.get('sts_token'):
+                params['apsarastack_access_key'] = auth.get('access_key_id')
+                params['apsarastack_secret_key'] = auth.get('access_key_secret')
+                params['security_token'] = auth.get('sts_token')
+                params['apsarastack_region'] = auth.get('region_id')
+                params = get_acs_connection_info(params)
+    elif params.get('apsarastack_assume_role_arn') or params.get('assume_role'):
+        params = get_assume_role(params)
+    else:
+        params = get_acs_connection_info(params)
+    return params
+
+
+def connect_to_acs(acs_module, region, **params):
+    conn = acs_module.connect_to_region(region, **params)
+    if not conn:
+        if region not in [acs_module_region.id for acs_module_region in acs_module.regions()]:
+            raise AnsibleACSError(
+                "Region %s does not seem to be available for acs module %s." % (region, acs_module.__name__))
+        else:
+            raise AnsibleACSError(
+                "Unknown problem connecting to region %s for acs module %s." % (region, acs_module.__name__))
+    return conn
+
+
+def ecs_connect(module):
+    """ Return an ecs connection"""
+    ecs_params = get_profile(module.params)
+    # If we have a region specified, connect to its endpoint.
+    region = module.params.get('apsarastack_region')
+    if region:
+        try:
+            ecs = connect_to_acs(footmark.ecs, region, **ecs_params)
+        except AnsibleACSError as e:
+            module.fail_json(msg=str(e))
+    # Otherwise, no region so we fallback to the old connection method
+    return ecs
+
+
+def vpc_connect(module):
+    """ Return an vpc connection"""
+    vpc_params = get_profile(module.params)
+    # If we have a region specified, connect to its endpoint.
+    region = module.params.get('apsarastack_region')
+    if region:
+        try:
+            vpc = connect_to_acs(footmark.vpc, region, **vpc_params)
+        except AnsibleACSError as e:
+            module.fail_json(msg=str(e))
+    # Otherwise, no region so we fallback to the old connection method
+    return vpc
