40b5a5609a6e6ec65595001453ed0af92f8bc065
刘栋 <ld01016998@alibaba-inc.com>
2025-03-12 18:02:46 +0800

    ecs_instance

---
+++ src/ansible_collections/alibaba/apsarastack/tests/test_ali_instance.py
@@ -0,0 +1,116 @@
+# encoding: utf-8
+'''
+Created on 2025年3月7日
+
+@author: jingyu.wy
+'''
+import uuid
+import unittest
+# import sys
+# sys.path.append(r"D:\code\ansible-module-apsarastack\src")
+
+from dotenv import load_dotenv
+
+from ansible_collections.alibaba.apsarastack.plugins.modules.ali_vpc import main as vpc_main
+from ansible_collections.alibaba.apsarastack.plugins.modules.ali_vswitch import main as vswtich_main
+from ansible_collections.alibaba.apsarastack.plugins.modules.ali_security_group import main as security_group_main
+from ansible_collections.alibaba.apsarastack.plugins.modules.ali_instance import main as instance_main
+from ansible_collections.alibaba.apsarastack.tests.test_utils import run_module, run_unittest_with_coverage
+
+
+class Test(unittest.TestCase):
+
+    def __init__(self, methodName:str="runTest") -> None:
+        unittest.TestCase.__init__(self, methodName=methodName)
+        self.name = "ansible_test_ecs_instance_info_%s" % uuid.uuid1()
+        self._vpc_args = {
+            "cidr_block": "172.16.0.0/16",
+            "vpc_name": self.name,
+            "description": "create by ansible unit test",
+        }
+        self._vswtich_args = {
+            "cidr_block": "172.16.1.0/24",
+            "name": self.name
+        }
+        self._security_group_args = {
+            "name": self.name
+        }
+
+    def setUp(self) -> None:
+        unittest.TestCase.setUp(self)
+        load_dotenv()
+        result = run_module(vpc_main, self._vpc_args)
+        # import pdb;pdb.set_trace()
+        self._vpc_args['id'] = result['vpc']['vpc_id']
+        self._vswtich_args["vpc_id"] = result['vpc']['vpc_id']
+        self._security_group_args["vpc_id"] = result['vpc']['vpc_id']
+        result = run_module(vswtich_main, self._vswtich_args)
+        self._vswtich_args["id"] = result['vswitch']['vswitch_id']
+        result = run_module(security_group_main, self._security_group_args)
+        self._security_group_args["id"] = result['group']["group_id"]
+
+    def tearDown(self) -> None:
+        unittest.TestCase.tearDown(self)
+        security_group = {
+            "state": "absent",
+        } | self._security_group_args
+        vswitch = {
+            "state": "absent",
+        } | self._vswtich_args
+        vpc_args = {
+            "state": "absent",
+        } | self._vpc_args
+        try:
+            run_module(security_group_main, security_group)
+        except:
+            pass
+        try:
+            run_module(vswtich_main, vswitch)
+            run_module(vpc_main, vpc_args)
+        except:
+            pass
+
+
+    def testCreateEcsInstance(self):
+        ecs_instance_args = {
+        "image": "arm_centos_7_6_20G_20211110.raw",
+        "instance_type": "ecs.g6x-ft-k10-c1m1.large",
+        "instance_name": "ansible_test_ecs_instance_test",
+        "vswitch_id": self._vswtich_args["id"],
+        "host_name": "ansible_test_ecs_instance_test",
+        "password": "Ld123@123",
+        "count_tag": "test_0312",
+        "system_disk_name": "ansible_test_ecs_instance_test",
+        "system_disk_category": "cloud_ssd",
+        "system_disk_size": "20",
+        "instance_charge_type": "PrePaid",
+        "security_groups": [self._security_group_args["id"]]
+            }
+        result = run_module(instance_main, ecs_instance_args)
+        self.assertNotIn('failed', result)
+        self.assertEqual(result['changed'], False)
+        self.assertEqual(result['instances'][0]["hostname"], ecs_instance_args["host_name"])
+
+        ecs_instance_args = {
+            "state": "absent",
+            "instance_ids": [result['instances'][0]["id"]]
+        }
+        result = run_module(instance_main, ecs_instance_args)
+        self.assertNotIn('failed', result, result.get('msg', ''))
+        
+        # vpc_args = {
+        #     "state": "present",
+        #     "cidr_block": "192.168.0.0/24",
+        #     "vpc_name": "ansible_test_vpc",
+        #     "description": "create by ansible unit test",
+        # }
+        # result = run_module(vpc_main, vpc_args)
+        # self.assertNotIn('failed', result, result.get('msg', ''))
+        # self.assertEqual(result['vpc']['vpc_name'], vpc_args['vpc_name'])
+        
+        
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testCreateVpc']
+    run_unittest_with_coverage()
